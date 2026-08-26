3685725b3e6e9b3f93a5da9cb1c005d686e12592
Cristian <cchura.cpc@gmail.com>
2026-08-25 23:02:26 -0400

    más consultas dml

--- ddl-dml.sql
+++ ddl-dml.sql
@@ -39,3 +39,117 @@ VALUES
 ('Informática', 'Libros relacionados con computación, programación y tecnología'),
 ('Historia', 'Libros sobre acontecimientos y períodos históricos'),
 ('Ciencias', 'Libros relacionados con ciencias naturales, física, química y biología');
+
+Pregunta 9 — Insertar libros
+Registra los siguientes libros:
+• Cien años de soledad
+• Introducción a MySQL
+• Historia de Bolivia
+• Programación Web
+• Ciencia y tecnología
+
+Recuerda utilizar categoria_id para relacionar cada libro con su categoría.
+
+id	titulo	anio_publicacion	editorial	categoria_id	numero_pagina	
+
+INSERT INTO libros (titulo, anio_publicacion, editorial, categoria_id, numero_pagina) 
+VALUES
+('Cien años de soledad', 1967, 'Editorial Sudamericana', 1, 471),
+('Introducción a MySQL', 2020, 'Alfaomega', 2, 320),
+('Historia de Bolivia', 2016, 'Editorial Juventud', 3, 450),
+('Programación Web', 2021, 'Alfaomega', 2, 380),
+('Ciencia y tecnología', 2019, 'Editorial Universitaria', 4, 300);
+
+
+Pregunta 11 — Registrar ejemplares
+Registra ejemplares para los libros de la biblioteca.
+Algunos libros deben tener más de una copia.
+Utiliza los siguientes estados según corresponda:
+• DISPONIBLE
+• MANTENIMIENTO
+
+INSERT INTO ejemplares
+(isbn, ubicacion, estado_fisico_libro, libro_id, disponible)
+VALUES
+('9780307474728', 'Estante A-01', 'DISPONIBLE', 1, TRUE),
+('9780307474729', 'Estante A-01', 'DISPONIBLE', 1, TRUE),
+
+('9788441532100', 'Estante B-01', 'DISPONIBLE', 2, TRUE),
+('9788441532101', 'Estante B-01', 'MANTENIMIENTO', 2, FALSE),
+
+('9789995412345', 'Estante C-01', 'DISPONIBLE', 3, TRUE),
+
+('9788426734567', 'Estante B-02', 'DISPONIBLE', 4, TRUE),
+('9788426734568', 'Estante B-02', 'DISPONIBLE', 4, TRUE),
+
+('9786071512345', 'Estante D-01', 'MANTENIMIENTO', 5, FALSE);
+
+DML
+INSERT DE USUARIOS
+Pregunta 13 — Registrar usuarios
+Registra cuatro usuarios:
+• Carlos Chávez
+• María Mamani
+• Juan Pérez
+• Ana Quispe
+Para cada usuario registra:
+• nombres
+• apellidos
+• número de documento
+• teléfono
+• correo
+• estado
+Tres usuarios deben estar ACTIVO y uno INACTIVO
+
+INSERT INTO usuarios (nombres, apellidos, nro_documento, telefono, correo, genero, estado)
+VALUES
+('Carlos', 'Chávez', '132654', '+5912321222', 'carlos@mail.com', 'MASCULINO', true),
+('María', 'Mamani', '98645351', '+5916554555', 'maria@mail.com', 'FEMENINO', true),
+('Juan', 'Pérez', '8645521', '+5914515455', 'juan@mail.com', 'MASCULINO', true),
+('Ana', 'Quispe', '86452121', '+591875545', 'ana@mail.com', 'FEMENINO', true);
+
+
+DML
+INSERT DE PRÉSTAMOS
+Pregunta 15 — Registrar préstamos
+Registra tres préstamos realizados por diferentes usuarios.
+Condiciones:
+• Un préstamo debe estar DEVUELTO.
+• Dos préstamos deben estar PRESTADO.
+• El préstamo devuelto debe tener registrada su fecha_devolucion_real.
+• Los préstamos todavía prestados deben tener fecha_devolucion_real en NULL.
+
+
+
+----------------------------------------------------------
+# RESET
+----------------------------------------------------------
+TRUNCATE categorias;
+id iniciará desde el 1
+
+DELETE FROM categorias;
+ALTER TABLE categorias AUTO_INCREMENT = 1;
+
+DELETE FROM libros;
+ALTER TABLE libros AUTO_INCREMENT = 1;
+
+DELETE FROM ejemplares;
+ALTER TABLE ejemplares AUTO_INCREMENT = 1;
+
+
+PLSQL
+PGPLSQL
+
+INDICES
+PROCEDIMIENTOS ALMACENADOS
+FUNCIONES
+VISTAS
+TRIGGERS
+CURSORES
+
+
+https://laravue2.blumbit.net/back/public/api/producto? page=6 & limit= &q=
+
+OFFSET
+LIMIT
+
